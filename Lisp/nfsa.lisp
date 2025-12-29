dde22b1bcaf55a31748bdd18802f0b1fba65ef34
JacopoFerrara <107486703+JacopoFerrara@users.noreply.github.com>
2025-12-29 16:39:18 +0100

    Add files via upload

--- Lisp/nfsa.lisp
+++ Lisp/nfsa.lisp
@@ -1,11 +1,10 @@
 ;Ferrara	Jacopo	924022
 
 (defstruct automa
-stato_iniziale
-stato_finale
+stato-iniziale
+stato-finale
 delta
 )
-
 
 
 (defun is-regex (RE)
@@ -15,14 +14,35 @@ delta
 ))
 
 
-
-
 (defun nfsa-compile-regex (RE)
-	(if (is-regex RE) 
-	(setq nuovoAutoma (make-automa: 
-	:stato_iniziale ))
-	(NIL)
-)
+	(if (is-regex RE)
+		(let ((iniziale (gensym))
+		(finale (gensym)))
+	(cond ((atom RE) 
+		(make-automa
+		:stato_iniziale iniziale
+		:stato_finale finale
+		:delta (list(list iniziale RE finale))))
+	((eq (first RE) 'c)
+	(let ((automi (mapcar #'nfsa-compile-regex (rest RE))))
+		(make-automa
+		:stato_iniziale (automa-stato-iniziale ((car) (first automi)))
+		:stato_finale (automa-stato-finale ((cdr) (last automi)))
+		:delta ((apply #'append (mapcar #'(automa-delta))) append ())	
+	))
+	((eq (first RE) 'a)
+	
+	)
+	((eq (first RE) 'z)
+	
+	
+	)
+	((eq (first RE) 'z)
+	
+	
+	))
+	NIL
+	)))
 
 
 (defun nfsa-recognize (FA input)
