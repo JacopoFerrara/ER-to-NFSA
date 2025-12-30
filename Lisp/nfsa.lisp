09b6a165866a4acaf4b9ee655f3a104793104453
JacopoFerrara <107486703+JacopoFerrara@users.noreply.github.com>
2025-12-30 12:19:18 +0100

    Compile Regex function done

--- Lisp/nfsa.lisp
+++ Lisp/nfsa.lisp
@@ -18,38 +18,52 @@ delta
 	(if (is-regex RE)
 		(let ((iniziale (gensym))
 		(finale (gensym)))
+	;atomo	
 	(cond ((atom RE) 
 		(make-automa
 		:stato-iniziale iniziale
 		:stato-finale finale
 		:delta (list(list iniziale RE finale))))
+	;concatenazione c
 	((eq (first RE) 'c)
 	(let ((automi (mapcar #'nfsa-compile-regex (rest RE))))
 		(make-automa
 		:stato-iniziale (automa-stato-iniziale (first automi))
 		:stato-finale (automa-stato-finale (first (last automi)))
-		:delta (append(apply #'append(mapcar #'automa-delta automi))(list(list (automa-stato-finale (first automi)) nil (automa-stato-iniziale (second automi)))))
-	))
+		:delta (append(apply #'append(mapcar #'automa-delta automi))
+		(list(list (automa-stato-finale (first automi)) nil (automa-stato-iniziale (second automi)))))))
+	;and a
 	((eq (first RE) 'a)
 	(let ((automi (mapcar #'nfsa-compile-regex (rest RE))))
 		(make-automa
 		:stato-iniziale iniziale
 		:stato-finale finale
-		:delta (append (apply #'append(mapcar #'automa-delta automi))(list(list iniziale nil (automa-stato-iniziale (first automi)))(list iniziale nil (automa-stato-iniziale (second automi)))(list (automa-stato-finale (first automi)) nil finale)(list (automa-stato-finale (second automi)) nil finale))
-	))
-	)
+		:delta (append (apply #'append(mapcar #'automa-delta automi))
+		(list(list iniziale nil (automa-stato-iniziale (first automi)))
+		(list iniziale nil (automa-stato-iniziale (second automi)))
+		(list (automa-stato-finale (first automi)) nil finale)
+		(list (automa-stato-finale (second automi)) nil finale)))))
+	;chiusura di Kleene z
 	((eq (first RE) 'z)
 	(let ((automi (mapcar #'nfsa-compile-regex (rest RE))))
 		(make-automa
-		:stato-iniziale (automa-stato-iniziale (first automi))
-		:stato-finale (automa-stato-finale (first (last automi)))
-		:delta (append(apply #'append(mapcar #'automa-delta automi))(list(list (automa-stato-finale (first automi)) nil (automa-stato-iniziale (second automi)))))
-	))
-	)
+		:stato-iniziale iniziale
+		:stato-finale finale
+		:delta (append(apply #'append(mapcar #'automa-delta automi))
+		(list(list iniziale nil (automa-stato-iniziale (first automi)))
+		(list iniziale nil finale)(list (automa-stato-finale (first automi)) nil (automa-stato-iniziale (first automi)))
+		(list (automa-stato-finale (first automi)) nil finale))))))
+	;or o 
 	((eq (first RE) 'o)
-	
-	
-	))
+	(let ((automi (mapcar #'nfsa-compile-regex (rest RE))))
+		(make-automa
+		:stato-iniziale iniziale
+		:stato-finale finale
+		:delta (append(apply #'append(mapcar #'automa-delta automi))
+		(list(list iniziale nil (automa-stato-iniziale (first automi)))
+		(list (automa-stato-finale (first automi)) nil finale)
+		(list iniziale nil finale))))))
+	)))
 	NIL
 	)))
 
